ccb9b07b83fa3fb66373b8a47d70cf2a9acda2d3
Michael Brade <brade@kde.org>
2015-10-09 09:26:34 -0700

    Fix the path and use insert, or it'll be an array in an array.
    New release.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Derby Lang Locale
 =================
+
+Simply set the `$locale.supported` model property to all translations set by derby-lang(-fs).
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = function () {
   return function (req, res, next) {
     var model = req.getModel();
-    var supported = Object.keys(model.get('$lang.translations'));
-    model.push('$locale.supported', supported);
+    var supported = Object.keys(model.get('$lang.dict.strings'));
+    model.insert('$locale.supported', 0, supported);
     next();
   };
 };
