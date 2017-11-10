6bf190fabf794f40f7cb8f5079d9b38673273940
Michael Brade <brade@kde.org>
2017-11-10 22:22:59 +0100

    req.getModel() is no more

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = function () {
   return function (req, res, next) {
-    var model = req.getModel();
+    var model = req.model;
+    if (!model) return next();
     var supported = Object.keys(model.get('$lang.dict.strings'));
     model.insert('$locale.supported', 0, supported);
     next();
